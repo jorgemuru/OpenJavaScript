a56f29778e6ff4f5411daece0d702c1ac6a399ee
jamuru <jamuru@gmail.com>
2022-07-08 21:45:18 -0300

    Ejercicio 2

---
+++ Ejercicio2/index.js
@@ -0,0 +1,16 @@
+//Declaro el objeto libro
+const miLibro = {
+    titulo: "El Señor de los Anillos",
+    autor: "J.R.Tolkien",
+    fecha: new Date(2000,01,31),
+    url: "www.senioranillos.com"
+};
+
+const nombre = "Jorge Martinez";
+const edad = 42;
+const desarrollador = true;
+fechaNacimiento = new Date(1979,11,27);
+
+const laLista = [nombre,edad,desarrollador,fechaNacimiento,miLibro];
+
+console.log(laLista);
